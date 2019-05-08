9ea9fde94c1517744024537e50f50666867fe7a0
ridesu <liyongzhimsn@hotmail.com>
2019-05-08 20:12:30 +0800

    ++ home

--- AngularSSR/ClientApp/src/app/app.module.ts
+++ AngularSSR/ClientApp/src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
